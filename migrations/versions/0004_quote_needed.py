5299ccfbf4ba4cda7a6055c24cde4061a70e62a2
Denis <dtsehtik@gmail.com>
2026-04-09 21:48:57 +0300

    Fix migration chain: 0004 references correct revision ID 0003

--- migrations/versions/0004_quote_needed.py
+++ migrations/versions/0004_quote_needed.py
@@ -9,7 +9,7 @@ from alembic import op
 import sqlalchemy as sa
 
 revision = "0004_quote_needed"
-down_revision = "0003_ai_assignment"
+down_revision = "0003"
 branch_labels = None
 depends_on = None
 
